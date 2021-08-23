7d5963cdc6b3eaeb7f0d1d24eebdab9f9264fd5f
Scott Barnes <scottreidbarnes@gmail.com>
2021-08-23 13:24:00 +0800

    Jinja 2 templates + mail working.

--- send_mail.py
+++ send_mail.py
@@ -1,9 +1,10 @@
-# Largely from https://realpython.com/python-send-email/#option-2-setting-up-a-local-smtp-server
+# Larhgely from https://realpython.com/python-send-email/#option-2-setting-up-a-local-smtp-server
 
 import smtplib, ssl
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
-import click
+# import click
+from jinja2 import Environment, FileSystemLoader, select_autoescape
 
 def send_mail(
     mail_server,
@@ -12,11 +13,21 @@ def send_mail(
     sender_email,
     password,
     subject,
+    template,
     body,
     cc=None,
-    bcc=None
+    bcc=None,
+    **kwargs,
 ):
     """ Send email. """
+
+    # Set up the Jinja2 template.
+    template_loader = FileSystemLoader(searchpath='./email_templates')
+    template_env = Environment(loader=template_loader)
+    # template = 'initial_team_email.jinja2'
+    template = template_env.get_template(template)
+    body = template.render(**kwargs)
+
     print(f'to: {to}')
     message = MIMEMultipart("alternative")
     message["Subject"] = subject
@@ -24,32 +35,29 @@ def send_mail(
     message["To"] = ', '.join(to)
     print(f'to: {message["To"]}')
 
-    # Create the plain-text and HTML version of your message
-    text = """\
-    Hi,
-    How are you?
-    Real Python has many great tutorials:
-    www.realpython.com"""
-    html = """\
-    <html>
-      <body>
-        <p>Hi,<br>
-           How are you?<br>
-           <a href="http://www.realpython.com">Real Python</a> 
-           has many great tutorials.
-        </p>
-      </body>
-    </html>
-    """
+    # # Create the plain-text and HTML version of your message
+    # text = """\
+    # This section is not used."""
+    # html = """\
+    # <html>
+    #   <body>
+    #     <p>Hi,<br>
+    #        This section is not used<br>
+    #     </p>
+    #   </body>
+    # </html>
+    # """
 
     # Turn these into plain/html MIMEText objects
-    part1 = MIMEText(text, "plain")
-    part2 = MIMEText(html, "html")
+    # part1 = MIMEText(text, "plain")
+    # part2 = MIMEText(html, "html")
+
+    email_content = MIMEText(body, "html")
 
     # Add HTML/plain-text parts to MIMEMultipart message
     # The email client will try to render the last part first
-    message.attach(part1)
-    message.attach(part2)
+    message.attach(email_content)
+    # message.attach(part2)
 
     # Create secure connection with server and send email
     context = ssl.create_default_context()
